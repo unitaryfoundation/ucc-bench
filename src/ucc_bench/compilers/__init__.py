9fb23aa4f7484629297be680d2bab3c49f4235a8
Brad Chase <brad@unitary.foundation>
2025-06-11 09:45:17 -0400

    Test MPS encoding

--- src/ucc_bench/compilers/__init__.py
+++ src/ucc_bench/compilers/__init__.py
@@ -4,6 +4,7 @@ from .qiskit_compiler import QiskitCompiler as QiskitCompiler
 from .cirq_compiler import CirqCompiler as CirqCompiler
 from .pytket_compiler import PytketPeepCompiler as PytketPeepCompiler
 from .ucc_compiler import UCCCompiler as UCCCompiler
+from .ucc_mps_compiler import UCCMPSCompiler as UCCMPSCompiler
 from .pyqpanda3_compiler import (
     PyQPanda3Compiler as PyQPanda3Compiler,
     PYQPANDA3_AVAILABLE as PYQPANDA3_AVAILABLE,
---
+++ src/ucc_bench/compilers/ucc_mps_compiler.py
@@ -0,0 +1,39 @@
+from .base_compiler import BaseCompiler
+from ucc import __version__ as ucc_version
+from ucc import compile
+
+# UCC uses qiskit internally
+## TODO: ucc should expose a circuit type vs. assuming always qiskit?
+from qiskit import QuantumCircuit
+from qiskit.transpiler import Target
+from typing import Optional
+from qbraid import transpile
+from ..registry import register
+
+
+@register.compiler("ucc+mps")
+class UCCMPSCompiler(BaseCompiler[QuantumCircuit]):
+    """
+    Wrapper for benchmarking ucc+MPS combined compiler.
+    """
+
+    @classmethod
+    def version(cls) -> str:
+        return ucc_version
+
+    def qasm_to_native(self, qasm: str) -> QuantumCircuit:
+        # Need to manually specifiy since id != "qiskit"
+        return transpile(qasm, "qiskit")
+
+    def compile(
+        self, circuit: QuantumCircuit, target_device: Optional[Target] = None
+    ) -> QuantumCircuit:
+        return compile(
+            circuit,
+            target_gateset={"rx", "ry", "rz", "h", "cx"},
+            target_device=target_device,
+            aqc=True,  # Enable AQC compilation
+        )
+
+    def count_multi_qubit_gates(self, circuit: QuantumCircuit) -> int:
+        return circuit.num_nonlocal_gates()
