cbf98715647991ab174697c8def16720a097792d
jordandsullivan <jordan@unitary.fund>
2025-06-30 15:47:17 -0700

    Move qec code into main qiskit circuits file

--- benchmarks/scripts/qiskit_circuits/circuits.py
+++ benchmarks/scripts/qiskit_circuits/circuits.py
@@ -15,7 +15,7 @@
 """Test circuit generation"""
 
 import numpy as np
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit.circuit import Parameter, ParameterVector
 from qiskit.circuit.library.standard_gates import XGate
 
@@ -229,4 +229,43 @@ def qaoa_ising_ansatz(num_qubits, num_layers):
         for i in range(num_qubits):
             qc.rx(2 * beta[layer], i)
 
+    return qc
+
+
+def qec_bitflip_code():
+    """
+    Returns a QuantumCircuit implementing the 3-qubit bit-flip code with error correction.
+    """
+    qreg_data = QuantumRegister(3, "data")
+    qreg_measure = QuantumRegister(2, "ancilla")
+    creg_data = ClassicalRegister(3, "c_data")
+    creg_syndrome = ClassicalRegister(2, "c_syndrome")
+    qc = QuantumCircuit(qreg_data, qreg_measure, creg_data, creg_syndrome)
+
+    # Initialize |1> state
+    qc.x(qreg_data[0])
+    qc.barrier(qreg_data)
+
+    # Encode bit-flip (repetition code)
+    qc.cx(qreg_data[0], qreg_data[1])
+    qc.cx(qreg_data[0], qreg_data[2])
+    qc.barrier(qreg_data)
+
+    # Syndrome measurement (parity checks)
+    qc.cx(qreg_data[0], qreg_measure[0])
+    qc.cx(qreg_data[1], qreg_measure[0])
+    qc.cx(qreg_data[0], qreg_measure[1])
+    qc.cx(qreg_data[2], qreg_measure[1])
+    qc.barrier(qreg_data, qreg_measure)
+    qc.measure(qreg_measure, creg_syndrome)
+    qc.barrier(qreg_data, qreg_measure)
+
+    # Correction (classically controlled)
+    qc.x(qreg_data[0]).c_if(creg_syndrome, 3)
+    qc.x(qreg_data[1]).c_if(creg_syndrome, 1)
+    qc.x(qreg_data[2]).c_if(creg_syndrome, 2)
+    qc.barrier(qreg_data)
+
+    # Final readout
+    qc.measure(qreg_data, creg_data)
     return qc
